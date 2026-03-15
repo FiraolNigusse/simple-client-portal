b9fe925d8b6f9b95f502b323367d7f9cf5ae0910
unknown <franigussie@gmail.com>
2026-03-15 22:50:12 +0300

    Add developer setup management command for easy testing

---
+++ backend/apps/users/management/commands/setup_dev.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from apps.users.models import Subscription
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    help = 'Create a developer account with Agency plan'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--email', type=str, help='Email for the dev account', default='dev@example.com')
+        parser.add_argument('--password', type=str, help='Password for the dev account', default='devpassword123')
+        parser.add_argument('--name', type=str, help='Name for the dev account', default='Developer Admin')
+
+    def handle(self, *args, **options):
+        email = options['email']
+        password = options['password']
+        name = options['name']
+
+        user, created = User.objects.get_or_create(
+            email=email,
+            defaults={
+                'name': name,
+                'is_staff': True,
+                'is_superuser': True,
+            }
+        )
+
+        if created:
+            user.set_password(password)
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Successfully created dev user: {email}'))
+        else:
+            self.stdout.write(self.style.WARNING(f'User {email} already exists. Promoting to superuser...'))
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+
+        # Set subscription to Agency
+        sub = Subscription.get_or_create_for_user(user)
+        sub.plan = Subscription.PLAN_AGENCY
+        sub.save()
+
+        self.stdout.write(self.style.SUCCESS(f'Subscription set to AGENCY for {email}'))
+        self.stdout.write(self.style.NOTICE(f'Login with: {email} / {password}'))
