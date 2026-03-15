e028319b34bebec5c7c9256bc182d5847d358fe9
unknown <franigussie@gmail.com>
2026-03-15 22:54:29 +0300

    Executing developer setup via build script for Render Free compatibility

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py setup_dev --email "admin@test.com" --password "TestPass123!" --name "Portal Admin"
