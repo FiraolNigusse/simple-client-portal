e84f586f62f6cdacd26eac1449cc4beb600954c3
unknown <franigussie@gmail.com>
2026-03-15 22:12:44 +0300

    Simplify production database config to correctly pick up Render env vars

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -6,7 +6,6 @@ DEBUG = False
 # Database
 DATABASES = {
     "default": dj_database_url.config(
-        default=config("DATABASE_URL"),
         conn_max_age=600,
         conn_health_checks=True,
     )
